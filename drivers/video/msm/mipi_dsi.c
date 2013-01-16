425833445d6281908aa0e7191157c8075ec11e76
Mayank Goyal <goyalm@codeaurora.org>
2013-01-16 22:39:54 +0530

    msm_fb: display: Avoid dsi clock warnings during boot
    Correctly set up the dsi_clk_rate in
    cont_splash_clk_ctrl before enabling them.
    
    Crs-fixed: 389531
    Change-Id: Iee13e6d871ca1fdb7f3f45a5e3b36784145abd86
    Signed-off-by: Mayank Goyal <goyalm@codeaurora.org>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -163,7 +163,6 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	fbi = mfd->fbi;
 	var = &fbi->var;
 	pinfo = &mfd->panel_info;
-	esc_byte_ratio = pinfo->mipi.esc_byte_ratio;
 
 	if (mipi_dsi_pdata && mipi_dsi_pdata->dsi_power_save)
 		mipi_dsi_pdata->dsi_power_save(1);
@@ -606,6 +605,11 @@ static int mipi_dsi_probe(struct platform_device *pdev)
 		goto mipi_dsi_probe_err;
 
 	pdev_list[pdev_list_cnt++] = pdev;
+
+	esc_byte_ratio = pinfo->mipi.esc_byte_ratio;
+
+	if (!mfd->cont_splash_done)
+		cont_splash_clk_ctrl(1);
 
 return 0;
 
