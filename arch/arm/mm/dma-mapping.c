c2b21fb8b2efbb74641541fade6ade5fdfd8c226
Laura Abbott <lauraa@codeaurora.org>
2013-01-16 18:23:19 -0800

    ARM: dma-mapping: Allow highmem pages to not have a mapping
    The DMA_ATTR_NO_KERNEL_MAPPING is used to make sure that CMA
    pages have no kernel mapping. Add support to make sure that
    highmem pages have no mapping.
    
    Change-Id: Ife76df126ecfedf0dba81a35e0de8a1787355b3d
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -505,6 +505,7 @@ static int __free_from_pool(void *start, size_t size)
 	return 1;
 }
 
+#define NO_KERNEL_MAPPING_DUMMY	0x2222
 static void *__alloc_from_contiguous(struct device *dev, size_t size,
 				     pgprot_t prot, struct page **ret_page,
 				     bool no_kernel_mapping,
@@ -525,10 +526,20 @@ static void *__alloc_from_contiguous(struct device *dev, size_t size,
 		__dma_remap(page, size, prot, no_kernel_mapping);
 		ptr = page_address(page);
 	} else {
-		ptr = __dma_alloc_remap(page, size, GFP_KERNEL, prot, caller);
-		if (!ptr) {
-			dma_release_from_contiguous(dev, page, count);
-			return NULL;
+		if (no_kernel_mapping) {
+			/*
+			 * Something non-NULL needs to be returned here. Give
+			 * back a dummy address that is unmapped to catch
+			 * clients trying to use the address incorrectly
+			 */
+			ptr = (void *)NO_KERNEL_MAPPING_DUMMY;
+		} else {
+			ptr = __dma_alloc_remap(page, size, GFP_KERNEL, prot,
+						caller);
+			if (!ptr) {
+				dma_release_from_contiguous(dev, page, count);
+				return NULL;
+			}
 		}
 	}
 	*ret_page = page;
@@ -541,7 +552,7 @@ static void __free_from_contiguous(struct device *dev, struct page *page,
 	if (!PageHighMem(page))
 		__dma_remap(page, size, pgprot_kernel, false);
 	else
-		__dma_free_remap(cpu_addr, size);
+		__dma_free_remap(cpu_addr, size, true);
 	dma_release_from_contiguous(dev, page, size >> PAGE_SHIFT);
 }
 
@@ -700,7 +711,7 @@ void arm_dma_free(struct device *dev, size_t size, void *cpu_addr,
 	} else if (__free_from_pool(cpu_addr, size)) {
 		return;
 	} else if (!IS_ENABLED(CONFIG_CMA)) {
-		__dma_free_remap(cpu_addr, size);
+		__dma_free_remap(cpu_addr, size, false);
 		__dma_free_buffer(page, size);
 	} else {
 		/*
