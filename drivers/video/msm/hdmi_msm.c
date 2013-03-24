15c6e8075d98521fcc3eb9ec9583ffa9969e2313
Ajay Singh Parmar <aparmar@codeaurora.org>
2013-03-24 15:04:16 +0530

    msm_fb: hdmi: Proper HPD state update
    On switching off the HPD, we need to update the HPD state
    after the polarity has been done to set the HPD state in
    hardware properly.
    
    CRs-Fixed: 465476
    Change-Id: Ic0ba7fac72b8188c86a034ba9a45107689809028
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/hdmi_msm.c
+++ drivers/video/msm/hdmi_msm.c
@@ -4792,8 +4792,6 @@ static int hdmi_msm_hpd_feature(int on)
 		rc = hdmi_msm_hpd_on();
 	} else {
 		if (external_common_state->hpd_state) {
-			external_common_state->hpd_state = 0;
-
 			/* Send offline event to switch OFF HDMI and HAL FD */
 			hdmi_msm_send_event(HPD_EVENT_OFFLINE);
 
@@ -4801,6 +4799,8 @@ static int hdmi_msm_hpd_feature(int on)
 			INIT_COMPLETION(hdmi_msm_state->hpd_event_processed);
 			wait_for_completion_interruptible_timeout(
 				&hdmi_msm_state->hpd_event_processed, HZ);
+
+			external_common_state->hpd_state = 0;
 		}
 
 		hdmi_msm_hpd_off();
