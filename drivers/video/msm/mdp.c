291ed9016f267f9d5b0d6957016a6660ceceade6
Sangani Suryanarayana Raju <csuryan@codeaurora.org>
2013-05-27 20:26:55 +0530

    msm: display: Proper handling of prepare and unprepare clocks
    This change handles prepare and unprepare clocks properly to
    fix clock mismatches.
    
    Change-Id: I09986aa1f0e1a5be2e07655a1db0d23760dd0725
    Signed-off-by: Sangani Suryanarayana Raju <csuryan@codeaurora.org>
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -3263,7 +3263,7 @@ void mdp_footswitch_ctrl(boolean on)
 	if (dsi_pll_vdda)
 		regulator_enable(dsi_pll_vdda);
 
-	mipi_dsi_prepare_clocks();
+	mipi_dsi_prepare_ahb_clocks();
 	mipi_dsi_ahb_ctrl(1);
 	mipi_dsi_phy_ctrl(1);
 	mipi_dsi_clk_enable();
@@ -3279,9 +3279,10 @@ void mdp_footswitch_ctrl(boolean on)
 	}
 
 	mipi_dsi_clk_disable();
+	mipi_dsi_unprepare_clocks();
 	mipi_dsi_phy_ctrl(0);
 	mipi_dsi_ahb_ctrl(0);
-	mipi_dsi_unprepare_clocks();
+	mipi_dsi_unprepare_ahb_clocks();
 
 	if (dsi_pll_vdda)
 		regulator_disable(dsi_pll_vdda);
