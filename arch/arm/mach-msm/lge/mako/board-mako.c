d5b3cb66986162d5abc492fd1b65d212a274f650
Steve Kondik <shade@chemlab.org>
2013-09-07 00:32:51 -0700

    mako/flo: Update for pm-8x60 changes
    Change-Id: I00aefae433570b3c7a499a1e3cc7593e044f89a9

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1779,7 +1779,6 @@ static struct platform_device *cdp_devices[] __initdata = {
 #ifdef CONFIG_MSM_ROTATOR
        &msm_rotator_device,
 #endif
-	   &msm8064_pc_cntr,
 	   &msm8960_cpu_slp_status,
 };
 
@@ -1921,11 +1920,12 @@ static void __init apq8064_common_init(void)
 	if (cpu_is_apq8064ab())
 		apq8064ab_update_krait_spm();
 	if (cpu_is_krait_v3()) {
-		msm_pm_set_tz_retention_flag(0);
+		struct msm_pm_init_data_type *pdata =
+				msm8064_pm_8x60.dev.platform_data;
+		pdata->retention_calls_tz = false;
 		apq8064ab_update_retention_spm();
-	} else {
-		msm_pm_set_tz_retention_flag(1);
 	}
+	platform_device_register(&msm8064_pm_8x60);
 	msm_spm_init(msm_spm_data, ARRAY_SIZE(msm_spm_data));
 	msm_spm_l2_init(msm_spm_l2_data);
 	msm_tsens_early_init(&apq_tsens_pdata);
