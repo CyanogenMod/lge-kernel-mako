830315f77303acb7ff5732348e40babdd241fedd
mrg666 <drgungor@hotmail.com>
2013-12-18 12:40:18 -0500

    Linux 3.4.25

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -255,6 +255,7 @@ extern int sysctl_tcp_cookie_size;
 extern int sysctl_tcp_thin_linear_timeouts;
 extern int sysctl_tcp_thin_dupack;
 extern int sysctl_tcp_default_init_rwnd;
+extern int sysctl_tcp_challenge_ack_limit;
 
 extern atomic_long_t tcp_memory_allocated;
 
