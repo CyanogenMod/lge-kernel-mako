a2533e660bd1e00650312ab4e0c546d8d88a2f41
mrg666 <drgungor@hotmail.com>
2014-01-08 09:08:57 -0500

    Linux 3.4.76
    	modified:   Makefile
    	modified:   arch/powerpc/include/asm/exception-64s.h
    	modified:   arch/powerpc/kernel/head_64.S
    	modified:   arch/powerpc/kvm/book3s_64_mmu_hv.c
    	modified:   arch/powerpc/kvm/book3s_hv_rm_mmu.c
    	modified:   arch/sh/lib/Makefile
    	modified:   drivers/ata/libata-core.c
    	modified:   drivers/dma/Kconfig
    	modified:   drivers/gpu/drm/drm_edid.c
    	modified:   drivers/gpu/drm/radeon/ni.c
    	modified:   drivers/hwmon/w83l786ng.c
    	modified:   drivers/idle/intel_idle.c
    	modified:   drivers/input/input.c
    	modified:   drivers/net/ethernet/broadcom/tg3.c
    	modified:   drivers/net/usb/dm9601.c
    	modified:   drivers/net/wireless/ath/ath9k/ar9002_mac.c
    	modified:   drivers/net/wireless/ath/ath9k/htc_drv_main.c
    	modified:   drivers/net/wireless/ath/ath9k/main.c
    	modified:   drivers/net/wireless/rtlwifi/pci.c
    	modified:   drivers/tty/serial/pmac_zilog.c
    	modified:   drivers/usb/class/cdc-wdm.c
    	modified:   drivers/usb/serial/generic.c
    	modified:   fs/ceph/addr.c
    	modified:   fs/ceph/mds_client.c
    	modified:   fs/gfs2/aops.c
    	modified:   fs/gfs2/ops_fstype.c
    	modified:   include/drm/drm_pciids.h
    	modified:   kernel/sched/debug.c
    	modified:   kernel/trace/ftrace.c
    	modified:   mm/rmap.c
    	modified:   net/wireless/radiotap.c
    	modified:   security/selinux/hooks.c
    	modified:   security/selinux/include/xfrm.h
    	modified:   security/selinux/xfrm.c
    	modified:   sound/core/pcm_lib.c
    	modified:   sound/soc/codecs/wm8904.c
    	modified:   tools/power/cpupower/utils/cpupower-set.c

--- drivers/net/ethernet/broadcom/tg3.c
+++ drivers/net/ethernet/broadcom/tg3.c
@@ -6619,7 +6619,7 @@ static inline int tg3_4g_overflow_test(dma_addr_t mapping, int len)
 {
 	u32 base = (u32) mapping & 0xffffffff;
 
-	return (base > 0xffffdcc0) && (base + len + 8 < base);
+	return base + len + 8 < base;
 }
 
 /* Test for DMA addresses > 40-bit */
