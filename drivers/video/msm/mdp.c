81e94bc0291701b5a17a60e936bc39571ab971ab
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-31 03:46:16 +0000

    msm_fb: display: Reapply LUT calibration when re-enabling MDP
    CONFIG_UPDATE_LCDC_LUT values were reset when MDP was shut down.
    Reapply them when coming back up to preserve the calibration
    
    Change-Id: I60327b03d401e4ddf86cb6efdb9990a5e5c41075

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -2424,6 +2424,10 @@ void mdp4_hw_init(void)
 
 static int mdp_bus_scale_restore_request(void);
 
+#if defined(CONFIG_MACH_APQ8064_MAKO) && defined(CONFIG_UPDATE_LCDC_LUT)
+extern int update_preset_lcdc_lut(void);
+#endif
+
 static int mdp_on(struct platform_device *pdev)
 {
 	int ret = 0;
@@ -2492,6 +2496,9 @@ static int mdp_on(struct platform_device *pdev)
 
 	pr_debug("%s:-\n", __func__);
 
+#if defined(CONFIG_MACH_APQ8064_MAKO) && defined(CONFIG_UPDATE_LCDC_LUT)
+	update_preset_lcdc_lut();
+#endif
 	return ret;
 }
 
