5f233efb0b95054ea202bac3b36e29c5e0f90444
mrg666 <drgungor@hotmail.com>
2014-02-13 11:27:20 -0500

    Linux 3.4.80

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5613,6 +5613,9 @@ void trigger_load_balance(struct rq *rq, int cpu)
 static void rq_online_fair(struct rq *rq)
 {
 	update_sysctl();
+
+	/* Ensure any throttled groups are reachable by pick_next_task */
+	unthrottle_offline_cfs_rqs(rq);
 }
 
 static void rq_offline_fair(struct rq *rq)
