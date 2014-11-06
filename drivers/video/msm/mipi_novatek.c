7a283d0026f47852ab043e449730f1eac5b2927f
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-06 19:46:52 +0000

    video: msm: mipi_novatek: Fix snprintf length usage

--- drivers/video/msm/mipi_novatek.c
+++ drivers/video/msm/mipi_novatek.c
@@ -680,7 +680,7 @@ static ssize_t mipi_dsi_3d_barrier_read(struct device *dev,
 				struct device_attribute *attr,
 				char *buf)
 {
-	return snprintf((char *)buf, sizeof(buf), "%u\n", barrier_mode);
+	return sprintf((char *)buf, "%u\n", barrier_mode);
 }
 
 static ssize_t mipi_dsi_3d_barrier_write(struct device *dev,
