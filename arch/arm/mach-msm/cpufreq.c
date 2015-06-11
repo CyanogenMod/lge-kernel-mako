fabbe3dcff136fdb02b1328ad799b7825530a034
Junjie Wu <junjiew@codeaurora.org>
2015-06-10 17:57:07 -0700

    cpufreq: Check current frequency in device driver
    __cpufreq_driver_target() checks if policy->cur is same as target_freq
    without holding any lock. This function is used by governor to
    directly set CPU frequency. Governor calling this function can't hold
    any CPUfreq framework locks due to deadlock possibility.
    
    However, this results in a race condition where one thread could see
    a stale policy->cur while another thread is changing CPU frequency.
    
    Thread A: Governor calls __cpufreq_driver_target(), starts increasing
    frequency but hasn't sent out CPUFREQ_POSTCHANGE notification yet.
    Thread B: Some other driver (could be thermal mitigation) starts
    limiting frequency using cpufreq_update_policy(). Every limits are
    applied to policy->min/max and final policy->max happens to be same as
    policy->cur. __cpufreq_driver_target() simply returns 0.
    Thread A: Governor finish scaling and now policy->cur violates
    policy->max and could last forever until next CPU frequency scaling
    happens.
    
    Shifting the responsibility of checking policy->cur and target_freq
    to CPUfreq device driver would resolve the race as long as the device
    driver holds a common mutex.
    
    Change-Id: I6f943228e793a4a4300c58b3ae0143e09ed01d7d
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -194,13 +194,16 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 				unsigned int target_freq,
 				unsigned int relation)
 {
-	int ret = -EFAULT;
+	int ret = 0;
 	int index;
 	struct cpufreq_frequency_table *table;
 
 	struct cpufreq_work_struct *cpu_work = NULL;
 
 	mutex_lock(&per_cpu(cpufreq_suspend, policy->cpu).suspend_mutex);
+
+	if (target_freq == policy->cur)
+		goto done;
 
 	if (per_cpu(cpufreq_suspend, policy->cpu).device_suspended) {
 		pr_debug("cpufreq: cpu%d scheduling frequency change "
