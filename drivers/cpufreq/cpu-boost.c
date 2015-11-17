e49cfd4c0fe234c12ed5df23a26dde20498b6cba
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-11-17 21:56:12 +0800

    cpufreq: cpu-boost: Remove hotplug thread infrastructure
    There is an upstream fix for this.
    
    Change-Id: Ic9c726d313d4b8b8c3a91d9af5c90fc0f77e30e0

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -20,13 +20,15 @@
 #include <linux/cpu.h>
 #include <linux/sched.h>
 #include <linux/jiffies.h>
-#include <linux/smpboot.h>
+#include <linux/kthread.h>
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
 
 struct cpu_sync {
+	struct task_struct *thread;
+	wait_queue_head_t sync_wq;
 	struct delayed_work boost_rem;
 	struct delayed_work input_boost_rem;
 	int cpu;
@@ -39,7 +41,6 @@ struct cpu_sync {
 };
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
-static DEFINE_PER_CPU(struct task_struct *, thread);
 static struct workqueue_struct *cpu_boost_wq;
 
 static struct work_struct input_boost_work;
@@ -125,16 +126,9 @@ static void do_input_boost_rem(struct work_struct *work)
 	cpufreq_update_policy(s->cpu);
 }
 
-static int boost_migration_should_run(unsigned int cpu)
+static int boost_mig_sync_thread(void *data)
 {
-	struct cpu_sync *s = &per_cpu(sync_info, cpu);
-
-	return s->pending;
-}
-
-static void run_boost_migration(unsigned int cpu)
-{
-	int dest_cpu = cpu;
+	int dest_cpu = (int) data;
 	int src_cpu, ret;
 	struct cpu_sync *s = &per_cpu(sync_info, dest_cpu);
 	struct cpufreq_policy dest_policy;
@@ -142,64 +136,66 @@ static void run_boost_migration(unsigned int cpu)
 	unsigned long flags;
 	unsigned int req_freq;
 
-	spin_lock_irqsave(&s->lock, flags);
-	s->pending = false;
-	src_cpu = s->src_cpu;
-	spin_unlock_irqrestore(&s->lock, flags);
+	while(1) {
+		wait_event(s->sync_wq, s->pending || kthread_should_stop());
 
-	ret = cpufreq_get_policy(&src_policy, src_cpu);
-	if (ret)
-		return;
+		if (kthread_should_stop())
+			break;
 
-	ret = cpufreq_get_policy(&dest_policy, dest_cpu);
-	if (ret)
-		return;
+		spin_lock_irqsave(&s->lock, flags);
+		s->pending = false;
+		src_cpu = s->src_cpu;
+		spin_unlock_irqrestore(&s->lock, flags);
 
-	req_freq = load_based_syncs ?
+		ret = cpufreq_get_policy(&src_policy, src_cpu);
+		if (ret)
+			continue;
+
+		ret = cpufreq_get_policy(&dest_policy, dest_cpu);
+		if (ret)
+			continue;
+
+		req_freq = load_based_syncs ?
 			(dest_policy.cpuinfo.max_freq * s->task_load) / 100 :
-								src_policy.cur;
+							src_policy.cur;
 
-	if (req_freq <= dest_policy.cpuinfo.min_freq) {
-	    pr_debug("No sync. Sync Freq:%u\n", req_freq);
-	    return;
+		if (req_freq <= dest_policy.cpuinfo.min_freq) {
+			pr_debug("No sync. Sync Freq:%u\n", req_freq);
+			continue;
+		}
+
+		if (sync_threshold)
+			req_freq = min(sync_threshold, req_freq);
+
+		cancel_delayed_work_sync(&s->boost_rem);
+
+		s->boost_min = req_freq;
+
+		/* Force policy re-evaluation to trigger adjust notifier. */
+		get_online_cpus();
+		if (cpu_online(src_cpu))
+			/*
+			 * Send an unchanged policy update to the source
+			 * CPU. Even though the policy isn't changed from
+			 * its existing boosted or non-boosted state
+			 * notifying the source CPU will let the governor
+			 * know a boost happened on another CPU and that it
+			 * should re-evaluate the frequency at the next timer
+			 * event without interference from a min sample time.
+			 */
+			cpufreq_update_policy(src_cpu);
+		if (cpu_online(dest_cpu)) {
+			cpufreq_update_policy(dest_cpu);
+			queue_delayed_work_on(dest_cpu, cpu_boost_wq,
+				&s->boost_rem, msecs_to_jiffies(boost_ms));
+		} else {
+			s->boost_min = 0;
+		}
+		put_online_cpus();
 	}
 
-	if (sync_threshold)
-		req_freq = min(sync_threshold, req_freq);
-
-	cancel_delayed_work_sync(&s->boost_rem);
-
-	s->boost_min = req_freq;
-
-	/* Force policy re-evaluation to trigger adjust notifier. */
-	get_online_cpus();
-	if (cpu_online(src_cpu))
-		/*
-		 * Send an unchanged policy update to the source
-		 * CPU. Even though the policy isn't changed from
-		 * its existing boosted or non-boosted state
-		 * notifying the source CPU will let the governor
-		 * know a boost happened on another CPU and that it
-		 * should re-evaluate the frequency at the next timer
-		 * event without interference from a min sample time.
-		 */
-		cpufreq_update_policy(src_cpu);
-	if (cpu_online(dest_cpu)) {
-		cpufreq_update_policy(dest_cpu);
-		queue_delayed_work_on(dest_cpu, cpu_boost_wq,
-			&s->boost_rem, msecs_to_jiffies(boost_ms));
-	} else {
-		s->boost_min = 0;
-	}
-	put_online_cpus();
+	return 0;
 }
-
-static struct smp_hotplug_thread cpuboost_threads = {
-	.store		= &thread,
-	.thread_should_run = boost_migration_should_run,
-	.thread_fn	= run_boost_migration,
-	.thread_comm	= "boost_sync/%u",
-};
 
 static int boost_migration_notify(struct notifier_block *nb,
 				unsigned long unused, void *arg)
@@ -223,7 +219,7 @@ static int boost_migration_notify(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	/* Avoid deadlock in try_to_wake_up() */
-	if (thread == current)
+	if (s->thread == current)
 		return NOTIFY_OK;
 
 	pr_debug("Migration: CPU%d --> CPU%d\n", mnd->src_cpu, mnd->dest_cpu);
@@ -232,6 +228,7 @@ static int boost_migration_notify(struct notifier_block *nb,
 	s->src_cpu = mnd->src_cpu;
 	s->task_load = load_based_syncs ? mnd->load : 0;
 	spin_unlock_irqrestore(&s->lock, flags);
+	wake_up(&s->sync_wq);
 
 	return NOTIFY_OK;
 }
@@ -372,21 +369,17 @@ static int cpu_boost_init(void)
 	for_each_possible_cpu(cpu) {
 		s = &per_cpu(sync_info, cpu);
 		s->cpu = cpu;
+		init_waitqueue_head(&s->sync_wq);
 		spin_lock_init(&s->lock);
 		INIT_DELAYED_WORK(&s->boost_rem, do_boost_rem);
 		INIT_DELAYED_WORK(&s->input_boost_rem, do_input_boost_rem);
+		s->thread = kthread_run(boost_mig_sync_thread, (void *)cpu,
+					"boost_sync/%d", cpu);
 	}
 	atomic_notifier_chain_register(&migration_notifier_head,
 					&boost_migration_nb);
 
-	ret = smpboot_register_percpu_thread(&cpuboost_threads);
-	if (ret)
-		pr_err("Cannot register cpuboost threads.\n");
-
 	ret = input_register_handler(&cpuboost_input_handler);
-	if (ret)
-		pr_err("Cannot register cpuboost input handler.\n");
-
-	return ret;
+	return 0;
 }
 late_initcall(cpu_boost_init);
