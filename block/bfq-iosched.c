6f1d54e0cf00de05421f40db1702e4275f3906f2
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-01-17 17:19:35 +0800

    block: bfq: Default to IOPs mode on SSDs
    Based on the similar commit for CFQ.
    
    Disabling slice_idle improves throughput on SSDs.
    
    Previously, we disabled slice_idle based on a prop set on boot. However,
    if/when the user switches schedulers, slice_idle gets set to 1 again.
    Disabling slice_idle in the kernel ensures that slice_idle is always
    disabled.
    
    Change-Id: I84212289240b45967bc0c860fb22f586c1c6f5d5

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -3886,6 +3886,18 @@ static void *bfq_init_queue(struct request_queue *q)
 	return bfqd;
 }
 
+static void bfq_registered_queue(struct request_queue *q)
+{
+	struct elevator_queue *e = q->elevator;
+	struct bfq_data *bfqd = e->elevator_data;
+
+	/*
+	 * Default to IOPS mode with no idling for SSDs
+	 */
+	if (blk_queue_nonrot(q))
+		bfqd->bfq_slice_idle = 0;
+}
+
 static void bfq_slab_kill(void)
 {
 	if (bfq_pool != NULL)
@@ -4149,6 +4161,7 @@ static struct elevator_type iosched_bfq = {
 		.elevator_may_queue_fn =	bfq_may_queue,
 		.elevator_init_fn =		bfq_init_queue,
 		.elevator_exit_fn =		bfq_exit_queue,
+		.elevator_registered_fn =	bfq_registered_queue,
 	},
 	.icq_size =		sizeof(struct bfq_io_cq),
 	.icq_align =		__alignof__(struct bfq_io_cq),
