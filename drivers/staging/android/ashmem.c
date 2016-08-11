756c08a91b62e32b042f2cf9ea1838efae7f933e
Marcho Markov <marcho.markov@gmail.com>
2016-08-11 14:27:59 +0200

    ashmem: Underp ashmem_fops
    Change-Id: I0c2bbde5303aa95474b85e86e1f403cfe9188e9c

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -793,7 +793,7 @@ static const struct file_operations ashmem_fops = {
 	.llseek = ashmem_llseek,
 	.mmap = ashmem_mmap,
 	.unlocked_ioctl = ashmem_ioctl,
-	.compat_ioctl = compat_ashmem_ioctl,
+	.compat_ioctl = ashmem_ioctl,
 };
 
 static struct miscdevice ashmem_misc = {
