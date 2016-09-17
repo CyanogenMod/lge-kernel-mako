2ed0a4c7209a35c0af9341ba80c6a91682cdd01e
zaclimon <isaacpateau05@gmail.com>
2016-09-17 09:08:51 -0400

    Revert "fs: ext4: disable support for fallocate FALLOC_FL_PUNCH_HOLE"
    This reverts commit 2407be6ec81d65348e2262095a3435a0db943845.
    
    Change-Id: I7497cbcc2100ae878d512b46776ffd045969d985

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3341,7 +3341,6 @@ int ext4_can_truncate(struct inode *inode)
 
 int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 {
-#if 0
 	struct inode *inode = file->f_path.dentry->d_inode;
 	if (!S_ISREG(inode->i_mode))
 		return -EOPNOTSUPP;
@@ -3355,13 +3354,8 @@ int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 		/* TODO: Add support for bigalloc file systems */
 		return -EOPNOTSUPP;
 	}
-#else
 
-	/*
-	 * Disabled as per b/28760453
-	 */
-	return -EOPNOTSUPP;
-#endif
+	return ext4_ext_punch_hole(file, offset, length);
 }
 
 /*
